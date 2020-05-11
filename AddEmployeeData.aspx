8248f17fac065cad2728b1bb6084994e3908f447
ShilpaPatil123 <shilpa.patil@nitorinfotech.com>
2020-05-11 16:33:01 +0530

    first commit

---
+++ AddEmployeeData.aspx
@@ -0,0 +1,88 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AddEmployeeData.aspx.cs" Inherits="AssignmentUsingListViewCrudOperation.AddEmployeeData" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+   
+        <fieldset style="width: 300px"><legend>ListView Example</legend>  
+        <asp:Table runat="server" Width="229px" >  
+            <asp:TableRow>  
+                <asp:TableCell>Name</asp:TableCell><asp:TableCell>
+                    <asp:TextBox runat="server" ID="txtName"></asp:TextBox></asp:TableCell>  
+            </asp:TableRow>  
+    
+              <asp:TableRow>  
+                <asp:TableCell>City</asp:TableCell>
+                  <asp:TableCell>
+                                      <asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True"   
+DataTextField="city" DataValueField="city" BackColor="#FFCCCC" Height="32px"   
+style="text-align: center" Width="145px">  
+                            <asp:ListItem Value="0">-- Select your City--</asp:ListItem>  
+                            <asp:ListItem>Rajkot</asp:ListItem>  
+                            <asp:ListItem>Ahmedabad</asp:ListItem>  
+                            <asp:ListItem>Bhuj</asp:ListItem>  
+                            <asp:ListItem>Surat</asp:ListItem>  
+                            <asp:ListItem>Delhi</asp:ListItem>  
+                        </asp:DropDownList>  
+               </asp:TableCell>  
+            </asp:TableRow>  
+            <asp:TableRow>  
+                <asp:TableCell>
+                       Gender  <asp:RadioButtonList ID="RadioButtonList1" runat="server" DataTextField="ans"   
+                    DataValueField="ans">  
+                    <asp:ListItem>Male</asp:ListItem>   <asp:ListItem>Female</asp:ListItem>  
+                </asp:RadioButtonList>  
+               </asp:TableCell>  
+            </asp:TableRow>  
+           <asp:TableRow>  
+               <asp:TableCell></asp:TableCell>  
+               <asp:TableCell>   
+                   <asp:Button runat="server" ID="btnSave" Text="Save" OnClick="btnSave_Click" />  
+                   <asp:Button runat="server" ID="btnUpdate" Text="Update" OnClick="btnUpdate_Click" />  
+               </asp:TableCell>  
+       </asp:TableRow>  
+      </asp:Table>  
+       </fieldset>  
+      
+  
+       <asp:HiddenField ID="hfRecord" runat="server" />  
+  
+  <h3>Employee Information</h3>  
+  
+        <asp:ListView ID="ListEmployee" runat="server" OnItemCommand="ListEmployee_ItemCommand" DataKeyNames="EmpID" >  
+        <itemtemplate>  
+       <table >  
+           <tr>  
+                        <td style =" width : 100px">  
+                            <b>No. :</b>  
+                            <%#Eval("EmpID") %>  
+                        </td>  
+                        <td style =" width : 200px">  
+                            <b>Name :</b>  
+                            <%#Eval("EmpName") %>  
+                        </td>  
+                        <td style =" width : 200px">  
+                            <b>City :</b>  
+                            <%#Eval("City") %>  
+                        </td >  
+                        <td style =" width : 250px">  
+                            <b>Gender :</b>  
+                            <%#Eval("Gender") %>  
+                        </td>  
+                    <td>  
+                        <asp:Button ID="btndel" runat="server" Text="Delete" tooltip="Delete a record" onclientclick="javascript:return confirm('Are you sure to delete record?')" CommandName="EmpDelete" CommandArgument='<%# DataBinder.Eval(Container.DataItem, "EmpID") %>' />  
+                        <asp:Button ID="btnupdt" runat="server" Text="Edit" tooltip="Update a record" CommandName="EmpEdit" CommandArgument='<%# DataBinder.Eval(Container.DataItem, "EmpID") %>' />  
+                    </td>  
+            </tr>  
+       </table>  
+       </itemtemplate>  
+    </asp:ListView>
+    </form>
+
+</body>
+</html>
---
+++ AddEmployeeData.aspx.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace AssignmentUsingListViewCrudOperation
+{
+    public partial class AddEmployeeData : System.Web.UI.Page
+    {
+        string connection = ConfigurationManager.ConnectionStrings["dbconnection"].ConnectionString;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                GetEmployeeDetail();
+            }
+        }
+        void GetEmployeeDetail()
+        {
+            SqlConnection con = new SqlConnection(connection);
+
+            SqlCommand cmd = new SqlCommand("sp_FillData", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            con.Open();
+            ListEmployee.DataSource = cmd.ExecuteReader();
+            ListEmployee.DataBind();
+        }
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection con = new SqlConnection(connection))
+            {
+                using (SqlCommand cmd = new SqlCommand("sp_InsertEmployeeData", con))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@EmpName", SqlDbType.VarChar).Value = txtName.Text.Trim();
+                    cmd.Parameters.AddWithValue("@City", SqlDbType.NChar).Value = DropDownList1.Text.Trim();
+                    cmd.Parameters.AddWithValue("@Gender", SqlDbType.NVarChar).Value = RadioButtonList1.Text.Trim();
+                    cmd.ExecuteNonQuery();
+                    con.Close();
+
+                    txtName.Text = String.Empty;
+                    DropDownList1.Text = String.Empty;
+                    RadioButtonList1.Text = String.Empty;
+                }
+            }
+            GetEmployeeDetail();
+        }
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(connection);
+            SqlCommand cmd = new SqlCommand("sp_UpdateEmployeeData", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@EmpID", Convert.ToInt32(hfRecord.Value));
+            cmd.Parameters.AddWithValue("@EmpName", SqlDbType.VarChar).Value = txtName.Text.Trim();
+            cmd.Parameters.AddWithValue("@City", SqlDbType.NChar).Value = DropDownList1.Text.Trim();
+            cmd.Parameters.AddWithValue("@Gender", SqlDbType.NVarChar).Value = RadioButtonList1.Text.Trim();
+
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+
+            GetEmployeeDetail();
+
+            btnSave.Visible = true;
+            btnUpdate.Visible = false;
+
+            hfRecord.Value = string.Empty;
+            txtName.Text = String.Empty;
+            DropDownList1.Text = String.Empty;
+            RadioButtonList1.Text = String.Empty;
+        }
+        protected void ListEmployee_ItemCommand(object sender, ListViewCommandEventArgs e)
+        {
+            switch (e.CommandName)
+            {
+                case ("EmpDelete"):
+                    int EmpID = Convert.ToInt32(e.CommandArgument);
+                    deleteEmployee(EmpID);
+                    break;
+                case ("EmpEdit"):
+                    EmpID = Convert.ToInt32(e.CommandArgument);
+                    UpdateEmployeeDetail(EmpID);
+                    break;
+            }
+        }
+        void deleteEmployee(int EmpID)
+        {
+            SqlConnection con = new SqlConnection(connection);
+            SqlCommand cmd = new SqlCommand("sp_DeleteEmployeeData", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+
+            cmd.Parameters.AddWithValue("@EmpID", EmpID);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
+            GetEmployeeDetail();
+        }
+        void UpdateEmployeeDetail(int EmpID)
+        {
+            SqlConnection con = new SqlConnection(connection);
+            SqlCommand cmd = new SqlCommand("sp_SelectEmployeeData", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@EmpID", EmpID);
+
+            con.Open();
+            SqlDataReader dr = cmd.ExecuteReader();
+            if (dr.HasRows)
+            {
+                dr.Read();
+                hfRecord.Value = dr["EmpID"].ToString();
+                txtName.Text = dr["EmpName"].ToString();
+                DropDownList1.Text = dr["City"].ToString();
+                RadioButtonList1.Text = dr["Gender"].ToString();
+            }
+            dr.Dispose();
+
+            con.Close();
+            btnSave.Visible = false;
+            btnUpdate.Visible = true;
+            GetEmployeeDetail();
+        }
+
+    }
+}
